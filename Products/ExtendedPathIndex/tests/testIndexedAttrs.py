9f58f9358601735570910707b2ede7753d46a2ca
Martijn Pieters <plone.org@zopatista.com>
2008-12-11 18:27:29 +0000

    Refactor:
    - Remove tests that test objects other than the index (such as ZCatalog)
    - Rewrite tests such that ZopeTestCase isn't needed, and rip it out.
    
    svn path=/Products.ExtendedPathIndex/trunk/; revision=23684

--- Products/ExtendedPathIndex/tests/testIndexedAttrs.py
+++ Products/ExtendedPathIndex/tests/testIndexedAttrs.py
@@ -2,150 +2,107 @@
 # IndexedAttrs tests
 #
 
-from Testing import ZopeTestCase
-
-ZopeTestCase.installProduct('ExtendedPathIndex')
-
-from Products.ZCatalog.ZCatalog import ZCatalog
-from OFS.SimpleItem import SimpleItem
+import unittest
 
 
-class Record:
-    def __init__(self, **kw):
-        self.__dict__.update(kw)
-
-class Dummy(SimpleItem):
-    def __init__(self, id):
-        self.id = id
+class MockItem(object):
+    def getId(self):
+        return 'mock'
+    def getPhysicalPath(self):
+        return ('', 'foo', 'bar', 'baz')
     def getCustomPath(self):
         return ('', 'custom', 'path')
     def getStringPath(self):
         return '/string/path'
 
 
-class TestIndexedAttrs(ZopeTestCase.ZopeTestCase):
-
-    def afterSetUp(self):
-        self.catalog = ZCatalog('catalog')
-        self.folder._setObject('dummy', Dummy('dummy'))
-        self.dummy = self.folder.dummy
-        self.physical_path = '/'.join(self.dummy.getPhysicalPath())
-        self.custom_path = '/'.join(self.dummy.getCustomPath())
-        self.string_path = self.dummy.getStringPath()
-
-    def addIndex(self, id='path', extra=None):
-        self.catalog.addIndex(id, 'ExtendedPathIndex', extra)
-        return self.catalog.Indexes[id]
-
-    def testAddIndex(self):
-        self.catalog.addIndex('path', 'ExtendedPathIndex')
-        try:
-            self.catalog.Indexes['path']
-        except KeyError:
-            self.fail('Failed to create index')
-
+class IndexedAttrsTests(unittest.TestCase):
+    def _makeOne(self, *args, **kw):
+        from Products.ExtendedPathIndex.ExtendedPathIndex import ExtendedPathIndex
+        return ExtendedPathIndex(*args, **kw)
+    
     def testDefaultIndexedAttrs(self):
         # By default we don't have indexed_attrs at all
-        idx = self.addIndex()
-        self.failIf(hasattr(idx, 'indexed_attrs'))
-
+        index = self._makeOne('path')
+        self.failIf(hasattr(index, 'indexed_attrs'))
+    
     def testDefaultIndexSourceNames(self):
         # However, getIndexSourceName returns 'getPhysicalPath'
-        idx = self.addIndex()
-        self.assertEqual(idx.getIndexSourceNames(), ('getPhysicalPath',))
-
+        index = self._makeOne('path')
+        self.assertEqual(index.getIndexSourceNames(), ('getPhysicalPath',))
+    
     def testDefaultIndexObject(self):
         # By default PathIndex indexes getPhysicalPath
-        idx = self.addIndex()
-        idx.index_object(123, self.dummy)
-        self.assertEqual(idx.getEntryForObject(123), self.physical_path)
-
-    def testDefaultSearchObject(self):
-        # We can find the object in the catalog by physical path
-        self.addIndex()
-        self.catalog.catalog_object(self.dummy)
-        self.assertEqual(len(self.catalog(path=self.physical_path)), 1)
-
-    def testDefaultSearchDictSyntax(self):
-        # PathIndex supports dictionary syntax for queries
-        self.addIndex()
-        self.catalog.catalog_object(self.dummy)
-        self.assertEqual(len(self.catalog(path={'query': self.physical_path})), 1)
-
+        index = self._makeOne('path')
+        index.index_object(123, MockItem())
+        self.assertEqual(index.getEntryForObject(123), '/foo/bar/baz')
+    
     def testExtraAsRecord(self):
-        # 'extra' can be a record type object
-        idx = self.addIndex(extra=Record(indexed_attrs='getCustomPath'))
-        self.assertEqual(idx.indexed_attrs, ('getCustomPath',))
-
+        # 'extra' can either be an instance
+        class Record(object):
+            def __init__(self, **kw):
+                self.__dict__.update(kw)
+        
+        index = self._makeOne('path', extra=Record(indexed_attrs='getCustomPath'))
+        self.assertEqual(index.indexed_attrs, ('getCustomPath',))
+    
     def testExtraAsMapping(self):
         # or a dictionary
-        idx = self.addIndex(extra={'indexed_attrs': 'getCustomPath'})
-        self.assertEqual(idx.indexed_attrs, ('getCustomPath',))
-
+        index = self._makeOne('path', extra=dict(indexed_attrs='getCustomPath'))
+        self.assertEqual(index.indexed_attrs, ('getCustomPath',))
+    
     def testCustomIndexSourceNames(self):
         # getIndexSourceName returns the indexed_attrs
-        idx = self.addIndex(extra={'indexed_attrs': 'getCustomPath'})
-        self.assertEqual(idx.getIndexSourceNames(), ('getCustomPath',))
-
+        index = self._makeOne('path', extra=dict(indexed_attrs='getCustomPath'))
+        self.assertEqual(index.getIndexSourceNames(), ('getCustomPath',))
+    
     def testCustomIndexObject(self):
         # PathIndex indexes getCustomPath
-        idx = self.addIndex(extra={'indexed_attrs': 'getCustomPath'})
-        idx.index_object(123, self.dummy)
-        self.assertEqual(idx.getEntryForObject(123), self.custom_path)
-
-    def testCustomSearchObject(self):
-        # We can find the object in the catalog by custom path
-        self.addIndex(extra={'indexed_attrs': 'getCustomPath'})
-        self.catalog.catalog_object(self.dummy)
-        self.assertEqual(len(self.catalog(path=self.custom_path)), 1)
-
+        index = self._makeOne('path', extra=dict(indexed_attrs='getCustomPath'))
+        index.index_object(123, MockItem())
+        self.assertEqual(index.getEntryForObject(123), '/custom/path')
+    
     def testStringIndexObject(self):
         # PathIndex accepts a path as tuple or string
-        idx = self.addIndex(extra={'indexed_attrs': 'getStringPath'})
-        idx.index_object(123, self.dummy)
-        self.assertEqual(idx.getEntryForObject(123), self.string_path)
-
-    def testStringSearchObject(self):
-        # And we can find the object in the catalog again
-        self.addIndex(extra={'indexed_attrs': 'getStringPath'})
-        self.catalog.catalog_object(self.dummy)
-        self.assertEqual(len(self.catalog(path=self.string_path)), 1)
-
+        index = self._makeOne('path', extra=dict(indexed_attrs='getStringPath'))
+        index.index_object(123, MockItem())
+        self.assertEqual(index.getEntryForObject(123), '/string/path')
+    
     def testIdIndexObject(self):
         # PathIndex prefers an attribute matching its id over getPhysicalPath
-        idx = self.addIndex(id='getId')
-        idx.index_object(123, self.dummy)
-        self.assertEqual(idx.getEntryForObject(123), 'dummy')
-
-    def testIdIndexObject(self):
+        index = self._makeOne('getId')
+        index.index_object(123, MockItem())
+        self.assertEqual(index.getEntryForObject(123), 'mock')
+    
+    def testIdAndIndexedAttrsObject(self):
         # Using indexed_attr overrides this behavior
-        idx = self.addIndex(id='getId', extra={'indexed_attrs': 'getCustomPath'})
-        idx.index_object(123, self.dummy)
-        self.assertEqual(idx.getEntryForObject(123), self.custom_path)
+        index = self._makeOne('getId', extra=dict(indexed_attrs='getCustomPath'))
+        index.index_object(123, MockItem())
+        self.assertEqual(index.getEntryForObject(123), '/custom/path')
 
     def testListIndexedAttr(self):
         # indexed_attrs can be a list
-        idx = self.addIndex(id='getId', extra={'indexed_attrs': ['getCustomPath', 'foo']})
+        index = self._makeOne('getId', extra=dict(indexed_attrs=['getCustomPath', 'foo']))
         # only the first attribute is used
-        self.assertEqual(idx.getIndexSourceNames(), ('getCustomPath',))
+        self.assertEqual(index.getIndexSourceNames(), ('getCustomPath',))
 
     def testStringIndexedAttr(self):
         # indexed_attrs can also be a comma separated string
-        idx = self.addIndex(id='getId', extra={'indexed_attrs': 'getCustomPath, foo'})
+        index = self._makeOne('getId', extra=dict(indexed_attrs='getCustomPath, foo'))
         # only the first attribute is used
-        self.assertEqual(idx.getIndexSourceNames(), ('getCustomPath',))
+        self.assertEqual(index.getIndexSourceNames(), ('getCustomPath',))
 
     def testEmtpyListAttr(self):
         # Empty indexed_attrs falls back to defaults
-        idx = self.addIndex(extra={'indexed_attrs': []})
-        self.assertEqual(idx.getIndexSourceNames(), ('getPhysicalPath',))
+        index = self._makeOne('getId', extra=dict(indexed_attrs=[]))
+        self.assertEqual(index.getIndexSourceNames(), ('getPhysicalPath',))
 
     def testEmtpyStringAttr(self):
         # Empty indexed_attrs falls back to defaults
-        idx = self.addIndex(extra={'indexed_attrs': ''})
-        self.assertEqual(idx.getIndexSourceNames(), ('getPhysicalPath',))
+        index = self._makeOne('getId', extra=dict(indexed_attrs=''))
+        self.assertEqual(index.getIndexSourceNames(), ('getPhysicalPath',))
 
 
 def test_suite():
-    import unittest, sys
+    import sys
     return unittest.findTestCases(sys.modules[__name__])
