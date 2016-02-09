cf3cf1461ad56f7adeade1ccbfd1c73d509f055c
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:44:52 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,33 +2,38 @@ from setuptools import setup, find_packages
 
 version = '3.1.1.dev0'
 
-setup(name='Products.ExtendedPathIndex',
-      version=version,
-      description="Zope catalog index for paths",
-      long_description=open("README.txt").read() + "\n" + \
-                       open("CHANGES.txt").read(),
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-        ],
-      keywords='Zope catalog index',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/Products.ExtendedPathIndex',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'AccessControl',
-          'transaction',
-          'ZODB3',
-          'Zope2 >= 2.13.0a3',
+setup(
+    name='Products.ExtendedPathIndex',
+    version=version,
+    description="Zope catalog index for paths",
+    long_description=(open("README.txt").read() + "\n" + \
+                      open("CHANGES.txt").read()),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
       ],
-      )
+    keywords='Zope catalog index',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/pypi/Products.ExtendedPathIndex',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'AccessControl',
+        'transaction',
+        'ZODB3',
+        'Zope2 >= 2.13.0a3',
+    ],
+    )
