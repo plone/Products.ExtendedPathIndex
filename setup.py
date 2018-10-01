495a5c63ea6dd9c9cd8187f0ebc7e0912cbc1c92
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:17:58 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     keywords='Zope catalog index',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/Products.ExtendedPathIndex',
+    url='https://pypi.org/project/Products.ExtendedPathIndex',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['Products'],
